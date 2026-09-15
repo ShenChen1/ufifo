dcc81e6ba913eec973e4b51b5d8458be14229abc
Peter Shen <peterchenshen@gmail.com>
2026-09-15 03:35:17 +0000

    feat(log2): extend API from uint to size_t

--- inc/log2.h
+++ inc/log2.h
@@ -1,34 +1,59 @@
 #ifndef _LOG2_H_
 #define _LOG2_H_
 
+#include <stddef.h>
+#include <stdint.h>
+
 /*
- * round up to nearest power of two
+ * Round up to nearest power of two.
+ *
+ * Returns 0 if:
+ *   - n == 0
+ *   - result cannot be represented by size_t
  */
 static inline __attribute__((const))
-unsigned int roundup_pow_of_two(unsigned int n)
+size_t roundup_pow_of_two(size_t n)
 {
+    if (n == 0)
+        return 0;
+
     n--;
+
     n |= n >> 1;
     n |= n >> 2;
     n |= n >> 4;
     n |= n >> 8;
     n |= n >> 16;
-    n++;
-    return n;
+
+#if SIZE_MAX > UINT32_MAX
+    n |= n >> 32;
+#endif
+
+    return n + 1;
 }
 
 /*
- * round down to nearest power of two
+ * Round down to nearest power of two.
+ *
+ * Returns 0 if n == 0.
  */
 static inline __attribute__((const))
-unsigned int rounddown_pow_of_two(unsigned int n)
+size_t rounddown_pow_of_two(size_t n)
 {
+    if (n == 0)
+        return 0;
+
     n |= n >> 1;
     n |= n >> 2;
     n |= n >> 4;
     n |= n >> 8;
     n |= n >> 16;
-    return (n + 1) >> 1;
+
+#if SIZE_MAX > UINT32_MAX
+    n |= n >> 32;
+#endif
+
+    return n - (n >> 1);
 }
 
 #endif /* _LOG2_H_ */
